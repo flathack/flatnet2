aed7fede33c967e8ff54c88d75032aa5083bdda4
BSSCHOE <BSSCHOE@IT605.de.Toyota-FS.com>
2016-02-01 15:11:13 +0100

    Die Übersicht hat nun mehr verwaltende Funktionen, die Navigation ist jetzt auch davon abhängig.

--- includes/uebersicht.class.php
+++ includes/uebersicht.class.php
@@ -9,11 +9,19 @@ include 'objekt/functions.class.php';
 class uebersicht extends functions {
 	
 	function mainUebersichtFunction() {
+		# Übersicht Manipulation
+		$this->changeStatusFunction();
+		
 		# Uebersicht anzeigen:
 		$this->showUebersicht();
 		
+		
+		
 		# Administrative-Funktionen
 		if($this->userHasRight("54", 0) == true) {
+			echo "<div class='mainbody'><a class='buttonlink' href='?newEntry'>Neue Kachel</a></div>";
+		}
+		if($this->userHasRight("54", 0) == true AND isset($_GET['newEntry'])) {
 			$this->showCreateEntity();
 		}
 		
@@ -107,18 +115,94 @@ class uebersicht extends functions {
 			echo "<input id='desc' type=text name=beschreibung value='" .$beschreibung. "' placeholder=Beschreibung />";
 			echo "<input id='sort' type=number name=sortierung value='" .$sortierung. "' placeholder=sort />";
 			echo "<input id='css' type=text name=css value='" .$css. "' placeholder=css />";
-			echo "<input id='right' type=number name=rightID value='" .$rightID. "' placeholder=Recht />";
+			
+			echo "<select name=rightID>";
+				$getAllRights = $this->getObjektInfo("SELECT * FROM userrights ORDER BY kategorie, id");
+				for($i = 0 ; $i < sizeof($getAllRights) ; $i++) {
+					echo "<option value='"; 
+						echo $getAllRights[$i]->id;
+					echo "'>";
+						echo $getAllRights[$i]->kategorie ." - ".$getAllRights[$i]->id ." - ". $getAllRights[$i]->recht;
+					echo "</option>";
+				}
+			echo "</select>";
+			
+		#	echo "<input id='right' type=number name=rightID value='" .$rightID. "' placeholder=Recht />";
 			echo "<input id='checkbox' type=checkbox checked value='" .$active. "' name=active /><label for=active>Aktiv</label>";
 			echo "<input id='submit' type=submit value=speichern />";
 		echo "</form></div>";
 	}
 	
 	/**
-	 * 
+	 * Führt die Änderung aus.
+	 */
+	function changeStatusFunction() {
+		if($this->userHasRight(52, 0) == true) {
+			if(isset($_POST['changeStatus'])) {
+			
+				$id = $_POST['id'];
+				$status = $_POST['changeStatus'];
+			
+				if($this->sql_insert_update_delete("UPDATE uebersicht_kacheln SET active=$status WHERE id=$id LIMIT 1") == true) {
+					echo "<p class='erfolg'>Wurde geändert</p>";
+				} else {
+					echo "<p class='meldung'>Es gab einen Fehler.</p>";
+				}
+			}
+		}
+		
+	}
+	
+	/**
+	 * Ändert den Status einer Kachel.
+	 * @param unknown $id
+	 */
+	function changeStatus($id) {
+		
+		if($this->userHasRight(52, 0) == true) {
+			$getKachelInfo = $this->getObjektInfo("SELECT * FROM uebersicht_kacheln WHERE id = $id");
+			
+			
+			
+			echo "<div class='changeStatus'><form method=post>";
+			echo "<input type=hidden name=id value=$id />";
+			
+			echo "<select name=changeStatus>";
+			
+			echo "<option"; 
+			if($getKachelInfo[0]->active == 1) {
+				echo " selected ";
+			}
+			echo " value=1>Aktiv</option>";
+			
+			echo "<option";
+			if($getKachelInfo[0]->active == 0) {
+				echo " selected ";
+			}
+			echo " value=0>Inaktiv</option>";
+			
+			echo "<option"; 
+			if($getKachelInfo[0]->active == 2) {
+				echo " selected ";
+			}
+			echo " value=2>gesperrt</option>";
+			
+			echo "</select>";
+			
+			echo "<input type=submit name=submit value=ok />";
+			echo "</form></div>";
+		}
+	}
+	
+	/**
+	 * Zeigt die Kacheln an.
 	 */
 	function showUebersicht() {
 		$kacheln = $this->getObjektInfo("SELECT * FROM uebersicht_kacheln WHERE active=1 ORDER BY sortierung, name, id");
+		$kachelnInactive = $this->getObjektInfo("SELECT * FROM uebersicht_kacheln WHERE active=0 ORDER BY sortierung, name, id");
+		$kachelnGesperrt = $this->getObjektInfo("SELECT * FROM uebersicht_kacheln WHERE active=2 ORDER BY sortierung, name, id");
 		
+		# Normale Kacheln
 		for($i = 0; $i < sizeof($kacheln); $i++) {
 			if($this->userHasRight($kacheln[$i]->rightID, 0) == true) {
 				echo "<div class=bereich".$kacheln[$i]->cssID.">";
@@ -127,10 +211,45 @@ class uebersicht extends functions {
 				echo "<p>";
 				echo $kacheln[$i]->beschreibung;
 				echo "</p>";
+				$this->changeStatus($kacheln[$i]->id);
 				echo "</div>";
 			
 			}
 		}
+		
+		# Bereiche, welche für den Benutzer nicht sichtbar sind.
+		for($i = 0; $i < sizeof($kacheln); $i++) {
+			if($this->userHasRight($kacheln[$i]->rightID, 0) == false) {
+				echo "<div class='bereichINACTIVE'>";
+				echo "<h2>".$kacheln[$i]->name."</h2>";
+				echo "<p>Dieser Inhalt ist zur Zeit für dich nicht verfügbar.</p>";
+				$this->changeStatus($kacheln[$i]->id);
+				echo "</div>";
+			}
+		}
+		
+		# Gesperrte Bereiche
+		for($i = 0; $i < sizeof($kachelnGesperrt); $i++) {
+			if($this->userHasRight($kachelnGesperrt[$i]->rightID, 0) == true) {
+				echo "<div class='bereichGesperrt'>";
+				echo "<h2>".$kachelnGesperrt[$i]->name."</h2>";
+				echo "<p>Der Inhalt wurde kurzfristig aufgrund eines Fehlers gesperrt. Versuche es später erneut.</p>";
+				$this->changeStatus($kachelnGesperrt[$i]->id);
+				echo "</div>";
+			}
+		}
+		
+		# Inaktive Bereiche
+		for($i = 0 ; $i < sizeof($kachelnInactive) ; $i++) {
+			if($this->userHasRight("52", 0) == true) {
+				echo "<div class='bereichINACTIVE'>";
+				echo "<h2>".$kachelnInactive[$i]->name."</h2>";
+				echo "<p>Dieser Inhalt ist deaktiviert, da er nicht fertig entwickelt ist oder die Entwicklung eingestellt wurde.</p>";
+				$this->changeStatus($kachelnInactive[$i]->id);
+				echo "</div>";
+			}
+		}
+			
 	}
 	
 }
