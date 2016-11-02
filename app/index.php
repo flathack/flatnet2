1ed093c328b776a48071b1112aeb5196d8600327
steve <steve@Steven-PC.local>
2016-11-02 17:29:25 +0100

    appdeleted

--- app/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-header("Location: /flatnet2/index.php");
-?>
--- app/voice3.php
+++
@@ -1,9 +0,0 @@
-<link href="app.css" type="text/css" rel="stylesheet" />
-
-<div class='appbox'>
-<div id="erfolg">
-<h1>Your message was sent to the frist aid assistants!</h1>
-</div>
-<a class="bottomRed" href="app.php">End</a>
-
-</div>
